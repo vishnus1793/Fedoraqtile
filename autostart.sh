2763b9eda39047040e5011f140f1fc6b71a7353c
vishnus1793 <vishnusakthi.44@gmail.com>
2025-03-08 12:08:25 +0530

    Changed the wallpaper

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
-feh --bg-fill /home/vishnu/Pictures/Wallpapers/bleach-ichigo.jpg &
+feh --bg-fill /home/vishnu/Pictures/Wallpapers/bg_12.jpg &
 
 polybar top &
 
 #!/bin/bash
 waybar &
+
+# Function to check if a process is already running and run it if not
+function run {
+  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
+  then
+    $@&
+  fi
+}
+
+
+# Uncomment the following line if you need to change your keyboard layout to Hebrew (IL)
+# setxkbmap -layout il
+
+
+#Uncomment the following line if you want to start sxhkd to replace Qtile native key-bindings
+#run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
+
+
+
+picom --config $HOME/.config/qtile/picom.conf &
+
+
+#starting user applications at boot time
+#run google-chrome-stable &
+#run volumeicon &
+# run thunar &  # Start Thunar file manager
+
+
+# Please be cautious while adding applications to start at boot time.
+# Only add applications you need on startup to avoid unnecessary resource usage.
